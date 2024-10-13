3464890c1faa0918e0f151b66e1b8792ad23672a
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-10-13 13:24:47 -0300

    Added actual code that generates the animation

--- animation-demo-clone/animation_demo_clone.py
+++ animation-demo-clone/animation_demo_clone.py
@@ -16,3 +16,31 @@ progress_bar = st.sidebar.progress(0)
 # for them using st.empty()
 frame_text = st.sidebar.empty()
 image = st.empty()
+
+m, n, s = 960, 640, 400
+x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
+y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
+
+for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
+    # Here were setting value for these two elements.
+    progress_bar.progress(frame_num)
+    frame_text.text("Frame %i/100" % (frame_num + 1))
+
+    # Performing some fractal wizardry.
+    c = separation * np.exp(1j * a)
+    Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
+    C = np.full((n, m), c)
+    M: Any = np.full((n, m), True, dtype=bool)
+    N = np.zeros((n, m))
+
+    for i in range(iterations):
+        Z[M] = Z[M] * Z[M] + C[M]
+        M[np.abs(Z) > 2] = False
+        N[M] = i
+
+    # Update the image placeholder by calling the image() function on it.
+    image.image(1.0 - (N / N.max()), use_column_width=True)
+
+# We clear elements by calling empty on them.
+progress_bar.empty()
+frame_text.empty()
