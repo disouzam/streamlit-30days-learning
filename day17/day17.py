a8fe3241ae51fd1d373f563e4fcbdab78fbe1b6f
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-10-14 14:12:14 -0300

    Example of acessing secrets in \`secrets.toml\` files

--- day17/day17.py
+++ day17/day17.py
@@ -3,9 +3,17 @@ import streamlit as st
 st.title('st.secrets')
 
 try:
-    if 'message' in st.secrets:
-        st.write(st.secrets['message'])
-    else:
-        st.write("There is no secret saved.")
+    if 'db_username' in st.secrets:
+        st.write(f"db_username: {st.secrets['db_username']}")
+
+    if 'db_password' in st.secrets:
+        st.write(f"db_password: {st.secrets['db_password']}")
+
+    if 'my_other_secrets' in st.secrets:
+        st.write(f"All other secrets: {st.secrets['my_other_secrets']}")
+
+    for item in st.secrets['my_other_secrets'].items():
+        st.write(item)
+    
 except:
     st.write("There is no secret saved.")
